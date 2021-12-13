19392037fef46ecc9a14f2956594826544f6fd39
gasxchenzho <77746930+gasxchenzho@users.noreply.github.com>
2021-12-13 03:01:25 -0500

    change_latest

--- src/App.js
+++ src/App.js
@@ -1,55 +1,52 @@
 import './App.css';
 import { useState } from "react"; 
 import axios from 'axios'
+import Highlighter from "react-highlight-words";
+
 
 
 function App() {
 
   const [pokemonName, setPokemonName] = useState("");
   const [indexerRes, setIndexderRes] = useState([]);
-  var data = require("./data.json");
+  
   // get the json data 
 
   function searchPokemon(){
+    
     //Indexer
-    axios.get(`https://pokeapi.co/api/v2/pokemon`).then((indexer) => {
+    axios.get(`http://ec2-107-23-251-43.compute-1.amazonaws.com:4567/hello/${pokemonName}`).then((indexer) => {
       
-      // globalResult = Res;
-      // var docList = [];
-      // for (let i = 0; i < Res.length; i++){
-      //   docList[i] = Res[i].name
-      // }
-      // console.log(indexer.data.results.name);
-      // console.log(indexer)
-      setIndexderRes(
-        indexer.data.results
-      );
-      // console.log(indexer);
       
 
-      // console.log(Res);
+
+
+
+
+      setIndexderRes(
+        indexer.data
+      )
+
+      indexerRes.sort((a,b) => (a.tf_idf   < b.tf_idf ) ? 1 : -1 )
+      console.log(indexerRes)
       
     })
     }
 
-  function showResults(){
-    // console.log(Res);
-    // console.log(pokemonRes);
-  }
-      
+  // function getHighlightedText(text, highlight) {
+  //     // Split on highlight term and include term into parts, ignore case
+  //     const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+  //     return <span> { parts.map((part, i) => 
+  //         <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? { fontWeight: 'bold',fontSize: 25 } : {} }>
+  //             { part }
+  //         </span>)
+  //     } </span>;
+  // }  
 
-      // pageRank
-    // axios.get(`https://pokeapi.co/api/v2/pokemon`).then((pagerank) => {
-    //   var PRRes = pagerank.data.results;
-    //   for (let i = 0; i < PRRes.length; i++){
-    //     // Res[i].score = ....
-    //   }
-    
-    // Res.sort(function(a, b) {
-    //     return a.score - b.score;
-    // }); 
-    
-    // })
+
+
+
+  
 
   return (
     <div className = "App">
@@ -57,23 +54,30 @@ function App() {
         <h1>Search it !</h1> 
         <input 
           type = "text" 
+
           onChange = {(event) => {
             setPokemonName(event.target.value);
+          
         }}
         />
         <button onClick = {() =>{
+          if(pokemonName != ""){
           searchPokemon();
+          }else{
+            setIndexderRes([]);
+          }
         }
         }>
         Search!
         </button>
-        <div>{console.log(indexerRes)}</div>
         <div className = "DisplaySection">
           {indexerRes.map((loc) => (
                 <div className = "res">
-                <div key={loc.name} style = {{color: "#85144b"}}>{loc.name}</div>
-                <div className = "url"><a href={loc.url} style = {{color: "#0074D9"}}>{loc.url}</a></div>
-                {/* <Link to={`/${loc}`} onClick={(anchor) => updateCity(anchor)}>{loc}</Link> */}
+                <div key={loc.url} style = {{color: "#85144b"}}>{loc.url}</div>
+                <div className = "url"><a href={loc.url} style = {{color: "#0074D9"}}>{loc.title}</a></div>
+                <div key = {loc.nearby_contents} style = {{color: "#85144b"}}>
+                  {loc.nearby_contents}
+                </div>
                 </div>
               ))}
         </div>
